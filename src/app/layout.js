20df97ded406d5e0ebe67f0b1be86650403420b7
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-09-10 13:30:40 -0700

    Creating a blog using next, context, hooks, SSR, CSR

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,33 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import NavBar from "@/components/NavBar/NavBar";
+import Footer from "@/components/Footer/Footer";
+import { ThemeContextProvider } from '@/context/ThemeContext';
+import ThemeProvider from '@/providers/ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
+  title: 'NextDev Blog',
   description: 'The best blog app!',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeContextProvider>
+          <ThemeProvider>
+            <div className="container">
+              <div className="wrapper">
+                <NavBar />
+                  {children}
+                <Footer />
+              </div>
+            </div>
+          </ThemeProvider>
+        </ThemeContextProvider>
+      </body>
     </html>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
+import Featured from "@/components/Featured/Featured";
+import CategoryList from "@/components/CategoryList/CategoryList";
+import Menu from "@/components/Menu/Menu";
+import CardList from "@/components/CardList/CardList";
+
 
 export default function Home() {
-  return <div>Hello</div>;
+  return ( 
+  <div className={styles.container}>
+    <Featured />
+    <CategoryList/>
+    <div className={styles.content}>
+      <CardList/>
+      <Menu/>
+    </div>
+  </div>
+  )
 }
